e98681ad60ff1bc2019fd96edd5b28e48593355d
Esra <nazifeesra98@gmail.com>
2023-06-27 21:17:06 +0300

    error fix 5 auth commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import GoogleProvider from "next-auth/providers/google"
 
 
 export const authOptions = {
+  secret:process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ,
