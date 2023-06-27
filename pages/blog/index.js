4e7254b09b8c80e3aebdaca720702ca0daeb1b26
Esra <nazifeesra98@gmail.com>
2023-06-27 21:53:33 +0300

    error fix 6 link w/ href commit

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -85,16 +85,16 @@ export default function Blogs() {
                       <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </Link>
                     {session?.user?.email=="nazifeesra98@gmail.com"  && (
-                    <Link onClick={()=>deleteBlog(blog._id)} class="inline-flex items-center font-medium text-primary-600 mt-5 hover:underline">
+                    <button onClick={()=>deleteBlog(blog._id)} class="inline-flex items-center font-medium text-primary-600 mt-5 hover:underline">
                       Yazıyı Sil
                       <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </Link>
+                    </button>
                     )}
                     {session?.user?.email=="ceren.basoglu.16@gmail.com"  && (
-                    <Link onClick={()=>deleteBlog(blog._id)} class="inline-flex items-center font-medium text-primary-600 mt-5 hover:underline">
+                    <button onClick={()=>deleteBlog(blog._id)} class="inline-flex items-center font-medium text-primary-600 mt-5 hover:underline">
                       Yazıyı Sil
                       <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </Link>
+                    </button>
                     )}
                 </Link>                 
                 ))}
